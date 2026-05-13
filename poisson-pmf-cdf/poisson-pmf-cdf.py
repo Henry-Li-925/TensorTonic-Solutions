06dbe5cfa115e0c9c7da75a6c0776ed339e61257
Zhe Li <144446607+Henry-Li-925@users.noreply.github.com>
2026-05-13 13:42:38 -0400

    Update solution for poisson-pmf-cdf

--- poisson-pmf-cdf/poisson-pmf-cdf.py
+++ poisson-pmf-cdf/poisson-pmf-cdf.py
@@ -4,7 +4,18 @@ def poisson_pmf_cdf(lam, k):
     """
     Compute Poisson PMF and CDF.
     """
-    # Write code here
-    pmf = np.exp(-lam + k * np.log(lam) - np.sum(np.log(np.arange(1, k+1))))
-    cdf = np.sum([np.exp(-lam + i * np.log(lam) - np.sum(np.log(np.arange(1, i+1)))) for i in range(k+1)])
+    def log_pmf(lam, k):
+        if k != 0:
+            k_seq = np.arange(1,k+1)
+        else:
+            k_seq = [1]        
+        def log_num(lam, k):
+            return -lam + k * np.log(lam)
+        def log_factorial(fac_seq):
+            return np.sum(np.log(fac_seq))
+        return log_num(lam,k) - log_factorial(k_seq)
+    pmf = np.exp(log_pmf(lam, k))
+
+    ind = np.arange(k+1)
+    cdf = np.sum([np.exp(log_pmf(lam, i)) for i in ind])
     return pmf, cdf
