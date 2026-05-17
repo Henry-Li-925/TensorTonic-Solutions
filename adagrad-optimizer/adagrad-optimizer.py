add4ed5fdd4971464ed4b18a13de691a2bc720dd
Zhe Li <144446607+Henry-Li-925@users.noreply.github.com>
2026-05-17 15:36:29 -0400

    Add solution for adagrad-optimizer

---
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
+    """
+    Perform one AdaGrad update step.
+    """
+    # Write code here
+    G_t = G + np.pow(g,2)
+    w_t = w - lr / np.sqrt(G_t + eps) * g
+    return w_t, G_t
